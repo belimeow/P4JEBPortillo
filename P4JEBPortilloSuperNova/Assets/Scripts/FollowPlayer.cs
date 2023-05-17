240ad632689a96d74f82ea19394ecfa2e2b1e6ff
belimeow <711592@student.dusd.net>
2023-05-17 12:16:56 -0700

    hi
    I started decorating the scene and made a bit of changes to the script

--- P4JEBPortilloSuperNova/Assets/Scripts/FollowPlayer.cs
+++ P4JEBPortilloSuperNova/Assets/Scripts/FollowPlayer.cs
@@ -16,6 +16,6 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position + new Vector3(0, 1, 0);
+        transform.position = player.transform.position + new Vector3(1, 1, 1);
     }
 }
--- P4JEBPortilloSuperNova/Assets/Scripts/PlayerController.cs
+++ P4JEBPortilloSuperNova/Assets/Scripts/PlayerController.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerController : MonoBehaviour
 {
+    NavMeshAgent agent;
 
     public float speed;
 
@@ -16,24 +18,25 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
-
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        verticalInput = Input.GetAxis("Vertical");
         transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
 
-
-        
-         
-
-        turn.x += Input.GetAxis("Mouse X");
-        turn.y += Input.GetAxis("Mouse Y");
-        transform.localRotation = Quaternion.Euler(-turn.y, turn.x, 0);
+        if(Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100)
+            {
+                agent.destination = hit.point;
+            }
+        }
     }
 }
