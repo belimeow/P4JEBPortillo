fff34a29aac0c1630904c9a3c71f1d0cda1b523c
belimeow <711592@student.dusd.net>
2023-05-23 12:01:33 -0700

    hi
    tried to make a script and tried to make it work to make trees spawn randomly

---
+++ P4JEBPortilloSuperNova/Assets/Scripts/SpawnRandom.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnRandom : MonoBehaviour
+{
+    public Tree prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float randomRange = Random.Range(-10.0f, 10.0f);
+        Vector3 randomPosition = new Vector3(randomRange, 0, 0);
+
+        var Tree = Instantiate(TreePrefab, randomPosition, Quaternion.identity);
+    }
+}
